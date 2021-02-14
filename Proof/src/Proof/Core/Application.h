eb61878842363bd2c39c77352b4848c82c77d934
marv nwadike <marvnwadike5@gmail.com>
2021-02-14 16:18:15 +0200

    nw

--- Proof/src/Proof/Core/Application.h
+++ Proof/src/Proof/Core/Application.h
@@ -7,7 +7,7 @@ namespace Proof {
 
 	 	class Proof_API Application {
 			public:
-						virtual ~Application();
+						virtual ~Application(); // main app
 						void Run();
 
 						void* GetWindow() {
