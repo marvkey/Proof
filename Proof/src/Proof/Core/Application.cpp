b6d1289e2610d47f192605869b464cd03a853a15
marv1191011910 <67913666+marv1191011910@users.noreply.github.com>
2021-02-15 18:01:44 +0200

    Update Application.cpp

--- Proof/src/Proof/Core/Application.cpp
+++ Proof/src/Proof/Core/Application.cpp
@@ -3,8 +3,8 @@
 
 
 namespace Proof {
-			WindowsWindow* Application::MainWindow = nullptr;
-			Application::Application() {
+		WindowsWindow* Application::MainWindow = nullptr;
+		Application::Application() {
 					
 	}
 	Application::~Application() {
@@ -16,22 +16,19 @@ namespace Proof {
 	void Application::Run() {
 		
 			
-			MainWindow = new WindowsWindow(200, 300);
-				PF_ENGINE_ERROR("WE ARE FINALLY BACK AFTER {} DAYS", 56);
+		MainWindow = new WindowsWindow(200, 300);
+		PF_ENGINE_ERROR("WE ARE FINALLY BACK AFTER {} DAYS", 56);
 				
+		MainWindow->createWindow();
+		bool newWindow = false;
+		float Num1 = 5;
+		while ((glfwGetKey( MainWindow->GetWindow(), KeyBoard::Escape) != KeyBoard::KeyPressed) && (glfwWindowShouldClose( MainWindow->GetWindow()) == false)) {
 
-				MainWindow->createWindow();
-				bool newWindow = false;
-				float Num1 = 5;
-				while ((glfwGetKey( MainWindow->GetWindow(), KeyBoard::Escape) != KeyBoard::KeyPressed) && (glfwWindowShouldClose( MainWindow->GetWindow()) == false)) {
-
-							 MainWindow->WindowLoop();
-
-								
-							 MainWindow->windowPollEvents(true);
-				};
-
-		 MainWindow->WindowEnd();
+			MainWindow->WindowLoop();
+					
+			MainWindow->windowPollEvents(true);
+		};
+			 MainWindow->WindowEnd();
 		
 		
 	}
